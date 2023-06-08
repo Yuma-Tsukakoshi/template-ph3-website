eab4f9c8cd7ce96c89b9b3a8c1a0125f8b733acf
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-08 11:26:07 +0900

    add:トップページ出力

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/index', function () {
+    return view('index');
 });
 
 Route::get('/dashboard', function () {
