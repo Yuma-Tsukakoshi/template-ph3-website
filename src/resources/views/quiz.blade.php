964533007c2adcad17b0b7bbe88714e2fe3aba65
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-09 11:40:51 +0900

    add: create questions_table

--- src/resources/views/quiz.blade.php
+++ src/resources/views/quiz.blade.php
@@ -1,28 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-top>
+    <x-slot name= 'title'>
+        ITクイズ | POSSE 初めてのWeb制作
+    </x-slot>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>ITクイズ | POSSE 初めてのWeb制作</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-    <link rel="stylesheet" href="{{asset('/assets/css/common.css')}}">
-    <link rel="stylesheet" href="{{asset('/assets/css/font.css')}}">
-    <script src="{{asset('/assets/js/common.js')}}" defer></script>
-    <script src="{{asset('/assets/js/quiz.js')}}" defer></script>
-</head>
-
-<body class="antialiased">
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
@@ -41,46 +21,7 @@
             </div>
         @endif
 
-        <header id="js-header" class="l-header p-header">
-            <div class="p-header__logo"><img src="{{asset('/assets/img/logo.svg')}}" alt="POSSE"></div>
-            <button class="p-header__button" id="js-headerButton"></button>
-            <div class="p-header__inner">
-                <nav class="p-header__nav">
-                    <ul class="p-header__nav__list">
-                        <li class="p-header__nav__item">
-                            <a href="./" class="p-header__nav__item__link">POSSEとは</a>
-                        </li>
-                        <li class="p-header__nav__item">
-                            <a href="." class="p-header__nav__item__link">クイズ</a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="p-header__official">
-                    <a href="https://line.me/R/ti/p/@651htnqp?from=page" target="_blank" rel="noopener noreferrer"
-                        class="p-header__official__link--line">
-                        <i class="u-icon__line"></i>
-                        <span class="">POSSE公式LINEを追加</span>
-                        <i class="u-icon__link"></i>
-                    </a>
-                    <a href="" class="p-header__official__link--website">POSSE 公式サイト<i class="u-icon__link"></i></a>
-                </div>
-                <ul class="p-header__sns p-sns">
-                    <li class="p-sns__item">
-                        <a href="https://twitter.com/posse_program" target="_blank" rel="noopener noreferrer"
-                            class="p-sns__item__link" aria-label="Twitter">
-                            <i class="u-icon__twitter"></i>
-                        </a>
-                    </li>
-                    <li class="p-sns__item">
-                        <a href="https://www.instagram.com/posse_programming/" target="_blank" rel="noopener noreferrer"
-                            class="p-sns__item__link" aria-label="instagram">
-                            <i class="u-icon__instagram"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </header>
-        <!-- /.l-header .p-header -->
+        
 
         <main class="l-main">
             <section class="p-hero p-quiz-hero">
@@ -194,6 +135,5 @@
         </footer>
         <!-- /.l-footer .p-footer -->
     </div>
-</body>
 
-</html>
+</x-top>
