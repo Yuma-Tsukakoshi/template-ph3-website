022759c75b3da6ef4a18a81759a5413267f66fcc
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-09 11:43:24 +0900

    add: create choices_table

--- src/database/migrations/2023_06_09_113546_create_choices_table.php
+++ src/database/migrations/2023_06_09_113546_create_choices_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('choices', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('question_id');
+            $table->string('name');
+            $table->boolean('valid');
             $table->timestamps();
         });
     }
