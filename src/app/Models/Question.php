a598d2288ed9715a272a96cae8035408a07aa5fc
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-09 12:05:37 +0900

    add: Question MOdel

--- src/app/Models/Question.php
+++ src/app/Models/Question.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Choice;
+
 
 class Question extends Model
 {
     use HasFactory;
+
+    public function questions()
+    {
+        return $this->hasMany(Choice::class);
+    }
 }
