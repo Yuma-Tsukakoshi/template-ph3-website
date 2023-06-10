a4d1dd854906aaa1afb143668d4120eb8bd28ffe
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-10 10:04:23 +0900

    add:ユーザー一覧

--- src/routes/web.php
+++ src/routes/web.php
@@ -36,7 +36,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+Route::get('/dashboard/user',[ProfileController::class, 'index']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
