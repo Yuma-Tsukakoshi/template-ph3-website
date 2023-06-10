b05ed6fde1b8e5529ee31161453011b42fd6a64d
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-10 18:44:13 +0900

    add:chenge branch

--- src/resources/views/layouts/aside.blade.php
+++ src/resources/views/layouts/aside.blade.php
@@ -1,8 +1,8 @@
 <aside class="z-20 flex-shrink-0 w-64 overflow-y-auto bg-slate-500 md:block">
   <div class="py-4 text-gray-500">
-    <a class="ml-6 text-lg font-bold text-gray-800 " href="#">
+    <span class="ml-6 text-lg font-bold text-gray-800 ">
       SideBanner
-    </a>
+    </span>
     <ul class="mt-6">
       <li class="relative px-6 py-3 mt-4">
         <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
