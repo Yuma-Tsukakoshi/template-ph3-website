f02fe356dad0655841e2d9928f8c6c5958f8836d
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-10 18:24:15 +0900

    add:ユーザーの管理画面

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -7,11 +7,35 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white w-3/5 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
             </div>
         </div>
     </div>
+
+    <x-slot name="table">
+        <table class="w-full whitespace-no-wrap">
+            <tbody class="bg-white divide-y">
+                <tr class="text-gray-700 text-3xl">
+                    <td class="px-4 py-3">
+                        <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+                            <h2 class="ml-4">ITクイズ一覧へ</h2>
+                        </a>
+                    </td>
+                    <td class="px-4 py-3">
+                        <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+                            <h2 class="ml-4">自己紹介クイズ一覧へ</h2>
+                        </a>
+                    </td>
+                    <td class="px-4 py-3">
+                        <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800" href="#">
+                            <h2 class="ml-4">ユーザー一覧へ</h2>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </x-slot>
 </x-app-layout>
--- src/resources/views/profile/index.blade.php
+++ src/resources/views/profile/index.blade.php
@@ -1,9 +1,45 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            ユーザー一覧
-        </h2>
-    </x-slot>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+          ユーザー
+      </h2>
+  </x-slot>
 
-    
+  <x-slot name="table">
+    <table class="w-full whitespace-no-wrap">
+      <thead>
+        <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b">
+          <th class="px-4 py-3">ユーザーID</th>
+          <th class="px-4 py-3">名前</th>
+          <th class="px-4 py-3">メールアドレス</th>
+          <th class="px-4 py-3">登録日時</th>
+          <th class="px-4 py-3">操作</th>
+        </tr>
+      </thead>
+      <tbody class="bg-white divide-y">
+        <tr class="text-gray-700">
+          @foreach ($users as $user)
+            <td class="px-4 py-3">
+              <p class="font-semibold items-center text-sm">{{ $user->id }}</p>
+            </td>
+            <td class="px-4 py-3">
+              <p class="font-semibold items-center text-sm">{{ $user->name}}</p>
+            </td>
+            <td class="px-4 py-3">
+              <p class="font-semibold items-center text-sm">{{ $user->email}}</p>
+            </td>
+            <td class="px-4 py-3">
+              <p class="font-semibold items-center text-sm">{{ $user->created_at}}</p>
+            </td>
+            <td class="px-4 py-3">
+              <div class="flex items-center space-x-4 text-sm">
+                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-blue-500 rounded-lg focus:outline-none focus:shadow-outline-gray"><a href="#">編集</a></button>
+                <button class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-gray-500 rounded-lg focus:outline-none focus:shadow-outline-gray"><a href="#">削除</a></button>
+              </div>
+            </td>
+          @endforeach
+        </tr>
+      </tbody>
+    </table>
+  </x-slot>
 </x-app-layout>
