18f5d6bc07662be311322806e8c4d44b84539e0c
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-15 11:45:01 +0900

    add:quizees msc

---
+++ src/app/Http/Controllers/QuizeesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreQuizeesRequest;
+use App\Http\Requests\UpdateQuizeesRequest;
+use App\Models\Quizees;
+
+class QuizeesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreQuizeesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Quizees $quizees)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Quizees $quizees)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateQuizeesRequest $request, Quizees $quizees)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Quizees $quizees)
+    {
+        //
+    }
+}
