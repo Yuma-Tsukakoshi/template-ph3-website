7e11b7a29ee90af0541cd3f834b8243e387f84e3
Yuma Tsukakoshi <yttn9984kawa@gmail.com>
2023-06-15 14:15:33 +0900

    add:3つのテーブルをeagerロードで結合＆カリキュラム終了

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use App\Models\Quizees;
 use Illuminate\Http\Request;
 use Laravel\Sail\Console\PublishCommand;
 
@@ -11,7 +12,9 @@ class QuizController extends Controller
     public function index($id)
     {
         //with ('紐付けたい関数名') をつける！今回は、Questionモデルのchoices関数を紐付けたいので、with('choices')となる。
+        $questions = Quizees::find($id);
         $questions = Question::with('choices')->get();
+        // $questions = Quizees::with('questions.choices')->find($id);
         // QuestionModelを1,2にわけて、idによって出力変更する
         // dd($questions);
         return view('quiz'.$id , compact('questions'));
